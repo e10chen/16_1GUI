f52a58cac61e16cb476f54bb4c3d46ac442b5c30
Ethan <ethanchen229@gmail.com>
2021-04-12 13:19:57 -0400

    16_1 GUI: Prototype your Real-World Data in a JavaFX GUI

---
+++ src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import javafx.scene.control.Accordion;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    public Accordion allGameData;
+    public TitledPane playStation4GameData;
+    public AnchorPane plyaStationAnchorData;
+    public TextArea playStation4Data;
+    public TitledPane aradeGameData;
+    public AnchorPane aradeAnchorData;
+    public TextArea aradeData;
+
+}
